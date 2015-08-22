327b5c1b9b4f593b17d46e7e40cc055e212b7527
Ryan Smyth <rynsmyth@gmail.com>
2015-08-21 18:10:23 -0600

    Lots o stuff

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,10 +2,28 @@ class ProfilesController < ApplicationController
   def new
     # form for each user to fill out there own profile
     @user = User.find(params[:user_id])
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
 
   def create
-
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success]='Profile Updated'
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:error]='Profile Not Created'
+      render action: :new
+    end
+  end
+  def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
   end
 end
+
+private
+def profile_params
+  params.require(:profile).permit(:first_name, :last_name, :contact_email, :description, :need)
+end
+
