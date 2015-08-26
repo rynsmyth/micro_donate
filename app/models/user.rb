6d4527422dd24cb86dfc2c1eeb80c66252869f97
Ryan Smyth <rynsmyth@gmail.com>
2015-08-25 19:55:45 -0600

    Misc Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :profile
+ 
+  has_one :profile
 end
